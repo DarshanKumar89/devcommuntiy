ccc5bb6652f8a21d9225cfc33629aaf88f057a82
Darshan kumar <darshankumar38@gmail.com>
2020-03-20 21:12:20 +0100

    added google analytics code updated: head true

--- gatsby-config.js
+++ gatsby-config.js
@@ -151,7 +151,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-133460649-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
