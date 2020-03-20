001317e6a638eb292c9f1a9714f765866e6323dc
Darshan kumar <darshankumar38@gmail.com>
2020-03-20 21:00:38 +0100

    added google analytics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
   siteMetadata: {
     title: `Darshan Kumar`,
     description: `full stack developer`,
-    copyrights: '',
+    copyrights: '2020',
     author: `@panr`,
     logo: {
       src: '',
@@ -144,6 +144,33 @@ module.exports = {
         domain: "https://darshankumar.netlify.com", // site domain
         key: "xxx" // pro-widget key
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-133460649-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "darshankumar.netlify.com",
+      },
     },
   ],
 }
--- src/pages/about.md
+++ src/pages/about.md
@@ -25,11 +25,5 @@ As a Apache PMC, I have contributed to open source repository.
 **Comapnies I woked with** :
  
 - [**Belgian Post Group**](https://en.wikipedia.org/wiki/Bpost)
+- Hortonworks 
 
-
-- **light/dark mode**, depending on your preferences
-- great reading experience thanks to [**Inter UI font**](https://rsms.me/inter/), made by [Rasmus Andersson](https://rsms.me/about/)
-- nice code highlighting thanks to [**PrismJS**](https://prismjs.com)
-- responsive youtube/vimeo etc. videos thanks to [**gatsby-remark-embed-video**](https://github.com/borgfriend/gatsby-remark-embed-video)
-
-So, there you have it... enjoy!
